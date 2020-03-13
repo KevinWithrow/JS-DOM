373ddf1507ab26cea5ed71680089faf6e66284aa
Kevin Withrow <kevinwithrow@Kevins-MacBook-Pro.local>
2020-03-12 20:07:29 -0500

    push everything

--- ex7/script.js
+++ ex7/script.js
@@ -1,3 +1,21 @@
 // Make the section disappear and reappear whenever you click the section-header
 // BONUS: Can you make the section slide up and down like this? :
 // https://www.w3schools.com/bootstrap/bootstrap_collapse.asp
+
+let clickableHeader = document.getElementById('section-header');
+
+clickableHeader.addEventListener('click', function () {
+  var content = document.getElementById('section');
+  if(content.style.display !== 'none'){
+    console.log (content.style.display)
+    content.style.display = 'none';
+}
+   
+  else{
+    console.log (content.style.display)
+    content.style.display = 'block';
+  }
+  
+});
+
+
--- ex8/script.js
+++ ex8/script.js
@@ -5,3 +5,24 @@
     BONUS) Add a functioning "delete" button to each task (HINT: use the keyword "this" in your delete button's click listener! Console log "this" to see what value it holds)
     SUPER BONUS) Make each task editable
 */
+
+
+let todoList = document.getElementById('todo-list');
+var button = document.getElementById('add-button');
+let input = document.getElementById('description');
+let ul = document.getElementById('todo-list');
+var deleteBtn = document.querySelector('.delete')
+
+button.onclick = function(){
+console.log(input.value);
+var newElement = document.createElement('li');
+newElement.innerHTML = input.value;
+newElement.classList.add('li');
+todoList.appendChild(newElement);
+};
+
+// delete items
+deleteBtn.addEventListener('click', function(){
+    localStorage.setItem('todo-list', ul.innerHTML);
+});
+
